5ce7e5f699a61c79ad555df70ca868fb85fb6e3f
shrirambansal <76605834+shrirambansal@users.noreply.github.com>
2023-06-25 21:56:01 +0530

    Create ex_4.py

---
+++ ex_4.py
@@ -0,0 +1,10 @@
+"""
+Write a program to print absolute vlaue of a number entered by user. E.g.-
+INPUT: 1 OUTPUT: 1
+INPUT: -1 OUTPUT: 1 
+"""
+num = float(input("Enter the Number :"))
+if num < 0:
+    print(num)
+else :
+    print(num)
