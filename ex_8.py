4682be55e3b68903b4393f46c4943328c6515d62
shrirambansal <76605834+shrirambansal@users.noreply.github.com>
2023-06-25 21:58:28 +0530

    Create ex_8.py

---
+++ ex_8.py
@@ -0,0 +1,9 @@
+"""
+Take values of length and breadth of a rectangle from user and check if it is square or not.
+"""
+length = int(input("Enter the Length : "))
+breadth = int(input("Enter the Breadth : "))
+if length == breadth :
+    print("Square")
+else:
+    print("not Square")
